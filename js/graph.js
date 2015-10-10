96b67bcd613ffd675d41e5f4faae42e393a50439
a-yang <angela.yang316@gmail.com>
2015-10-10 17:24:50 -0400

    Revised graph.js

--- js/graph.js
+++ js/graph.js
@@ -2,7 +2,9 @@ function getProfilePhoto(
 	FB.api('/me/picture',
 	function(response) {
 		if (response && !response.error) {
-
+			console.log("Got profile photo");
+		} else {
+			console.log("Could not access photo");
 		}
 	});
 }
